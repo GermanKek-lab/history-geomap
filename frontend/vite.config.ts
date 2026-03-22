9a53eae0816c9db19b3a47f3968c0950ba89840b
kekgerman <kekgerman81@gmail.com>
2026-03-22 19:50:03 +0300

    chore: bootstrap project structure and local development setup

---
+++ frontend/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    allowedHosts: [
+      "81ac-82-22-40-144.ngrok-free.app",
+    ],
+  },
+});
